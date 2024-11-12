69fad5fa949e54f088442fcd319f02ee09892c91
tara-angeline <161752134+tara-angeline@users.noreply.github.com>
2024-11-12 11:46:53 +0000

    Add files via upload
    Added .html files

---
+++ script.js
@@ -0,0 +1,31 @@
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+let searchBoxCancel = document.querySelector(".search-box .bx-x");
+searchBox.addEventListener("click", ()=>{
+  navbar.classList.toggle("showInput");
+  if(navbar.classList.contains("showInput")){
+    searchBox.classList.replace("bx-search" ,"bx-x");
+  }else {
+    searchBox.classList.replace("bx-x" ,"bx-search");
+  }
+});
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function() {
+navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+    navLinks.style.left = "-100%";
+    }
+    // sidebar submenu open close js code
+    let htmlcssArrow = document.querySelector(".arrow1");
+    htmlcssArrow.onclick = function() {
+     navLinks.classList.toggle("show1");
+    }
+    let jsArrow = document.querySelector(".arrow3");
+    jsArrow.onclick = function() {
+     navLinks.classList.toggle("show3");
+    }
